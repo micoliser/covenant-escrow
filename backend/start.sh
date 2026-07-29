3879ba4c1ba626bd9c619abdb8ba2f965c743f1c
Samuel Iwelumo <samueliwelumo@gmail.com>
2026-07-29 12:48:02 +0100

    chore: restrict Celery concurrency and Gunicorn workers

--- backend/start.sh
+++ backend/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Start Celery worker and beat in the background
-celery -A covenant_escrow_backend worker -B -l info &
+# Start Celery worker and beat in the background (restricted to 1 concurrent process to save RAM)
+celery -A covenant_escrow_backend worker -B -l info --concurrency=1 &
 
-# Start Gunicorn in the foreground
-gunicorn covenant_escrow_backend.wsgi:application
+# Start Gunicorn in the foreground (restricted to 1 worker to fit in the 512MB free tier limit)
+gunicorn covenant_escrow_backend.wsgi:application --workers 1 --threads 2
